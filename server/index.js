49a04e1935faf956257f76a4e98c89e96dda553c
Marc-Andre Auger <auger.747@gmail.com>
2026-04-28 21:21:50 -0400

    Update server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 import dotenv from "dotenv"
 dotenv.config()
-
+import path from "path"
 import express from "express"
 import axios from "axios"
 import cors from "cors"
 
 const app = express()
+const __dirname = path.resolve();
 
 // allow your React app to call this server
 app.use(cors())
-
+app.use(express.static(path.join(__dirname, "../dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
 const PORT = 3001
 
 app.get("/api/nhl", async (req, res) => {
